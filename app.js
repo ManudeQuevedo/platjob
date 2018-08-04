227311273780d70a6016dc35d6b6841c2d8b4e73
MyrnaArredondo <tracia_ra@hotmail.com>
2018-08-04 17:50:40 -0500

    vistas

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 require('dotenv').config();
 
-const bodyParser     = require('body-parser');
-const cookieParser   = require('cookie-parser');
-const express        = require('express');
-const favicon        = require('serve-favicon');
-const hbs            = require('hbs');
-const mongoose       = require('mongoose');
-const logger         = require('morgan');
-const path           = require('path');
-const session        = require('express-session');
-const bcrypt         = require('bcryptjs');
-const passport       = require('passport');
-const LocalStrategy  = require('passport-local').Strategy;
-const User           = require("./models/user");
-const flash          = require("connect-flash");
-const MongoStore     = require("connect-mongo")(session);
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require("./models/user");
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo")(session);
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/platjob', { 
-    useMongoClient: true 
-  })
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/platjob', {
+        useMongoClient: true
+    })
+    .then(() => {
+        console.log('Connected to Mongo!')
+    }).catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
-const app_name  = require('./package.json').name;
-const debug     = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-const app       = express();
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app = express();
 
 
 // Middleware Setup
@@ -42,9 +42,9 @@ app.use(cookieParser());
 
 // Express View engine setup
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -54,16 +54,16 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 6000
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // Keeps session open for 1 day
-  })
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 6000
+    },
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 24 * 60 * 60 // Keeps session open for 1 day
+    })
 }));
 
 // default value for title local
@@ -72,40 +72,40 @@ app.locals.title = 'PlatJob';
 
 // Passport config
 passport.serializeUser((user, cb) => {
-  cb(null, user._id);
+    cb(null, user._id);
 });
 
 passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
+    User.findById(id, (err, user) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, user);
+    });
 });
 
 app.use(flash());
 
 passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, {
-        message: "Incorrect password or username, please validate"
-      });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, {
-        message: "Incorrect password or username, please validate"
-      });
-    }
+    User.findOne({
+        username
+    }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(null, false, {
+                message: "Incorrect password or username, please validate"
+            });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+                message: "Incorrect password or username, please validate"
+            });
+        }
 
-    return next(null, user);
-  });
+        return next(null, user);
+    });
 }));
 
 //========================>
@@ -120,6 +120,6 @@ const index = require('./routes/index');
 app.use('/', index);
 
 const passportRouter = require('./routes/passportRouter');
-app.use('/', passportRouter);
+//app.use('/', passportRouter);
 
 module.exports = app;
