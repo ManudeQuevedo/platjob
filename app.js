44c1396713797c13e25b1d0cb30e3418a3e22e87
MyrnaArredondo <tracia_ra@hotmail.com>
2018-08-11 10:44:02 -0500

    cambio myrna

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require("./models/user");
@@ -19,14 +19,14 @@ const MongoStore = require("connect-mongo")(session);
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/platjob-2', {
-    useMongoClient: true
-  })
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/platjob-2', {
+        useMongoClient: true
+    })
+    .then(() => {
+        console.log('Connected to Mongo!')
+    }).catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -37,75 +37,75 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieParser());
 app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // Keeps session open for 1 day
-  })
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 24 * 60 * 60 // Keeps session open for 1 day
+    })
 }));
 
 passport.serializeUser((userDetails, cb) => {
-  cb(null, userDetails._id);
+    cb(null, userDetails._id);
 });
 
 passport.deserializeUser((id, cb) => {
-  User.findById(id)
-    .then((userDetails) => {
-      cb(null, userDetails);
-    })
-    .catch((err) => {
-      cb(err);
-    });
+    User.findById(id)
+        .then((userDetails) => {
+            cb(null, userDetails);
+        })
+        .catch((err) => {
+            cb(err);
+        });
 });
 
 app.use(flash());
 passport.use(new LocalStrategy({
-  passReqToCallback: true
+    passReqToCallback: true
 }, (req, username, password, next) => {
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, {
-        message: "Incorrect information, please try again"
-      });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, {
-        message: "Incorrect information, please try again"
-      });
-    }
+    User.findOne({
+        username
+    }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(null, false, {
+                message: "Incorrect information, please try again"
+            });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+                message: "Incorrect information, please try again"
+            });
+        }
 
-    return next(null, user);
-  });
+        return next(null, user);
+    });
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.locals.blahUser = req.user;
-  next();
+    res.locals.blahUser = req.user;
+    next();
 });
 
 
 // Express View engine setup
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
 
 
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,24 +1,24 @@
 // Passport configuration:
-const passport     = require("passport");
+const passport = require("passport");
 
-const express      = require("express");
-const authRoutes   = express.Router();
+const express = require("express");
+const authRoutes = express.Router();
 
 // Models
-const User         = require("../models/user");
+const User = require("../models/user");
 // const Company      = require("../models/company");
 // const Job          = require("../models/job");
 
 // Bcrypt to encrypt passwords
-const bcrypt       = require("bcrypt");
-const bcryptSalt   = 10;
+const bcrypt = require("bcryptjs");
+const bcryptSalt = 10;
 
 // /routes/auth-routes.js
-const ensureLogin  = require("connect-ensure-login");
+const ensureLogin = require("connect-ensure-login");
 
 // Validates roles to redirect to specific page:
-const checkRoles   = require("./validateRouter")
-//=====================================================================================>
+const checkRoles = require("./validateRouter")
+    //=====================================================================================>
 
 
 
@@ -27,14 +27,14 @@ const checkRoles   = require("./validateRouter")
 //=====================================================================================>
 // User Accesses:
 authRoutes.get("/login", (req, res, next) => {
-  res.render("auth-views/login", { "message": req.flash("error") });
+    res.render("auth-views/login", { "message": req.flash("error") });
 });
 
 authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/dashboard",
-  failureRedirect: "/login",
-  failureFlash: true,
-  passReqToCallback: true
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
 }));
 //=====================================================================================>
 
@@ -45,86 +45,86 @@ authRoutes.post("/login", passport.authenticate("local", {
 //=====================================================================================>
 // Sign Up Form:
 authRoutes.get("/signup", (req, res, next) => {
-  res.render("auth-views/signup");
+    res.render("auth-views/signup");
 });
 
 authRoutes.post("/signup", (req, res, next) => {
-  const role      = req.body.role;
-  const email     = req.body.email;
-  const firstname = req.body.firstname;
-  const lastname  = req.body.lastname;
-  const username  = req.body.username;
-  const password  = req.body.password;
+    const role = req.body.role;
+    const email = req.body.email;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const username = req.body.username;
+    const password = req.body.password;
 
-  if (username === "" || password === "" || role === "" || firstname === "" || email === "" || lastname === "") {
-    res.render("auth-views/signup", {
-      message: "All fields are mandatory."
-    });
-    return;
-  }
-
-  User.findOne({
-    username
-  }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth-views/signup", {
-        message: "The username already exists"
-      });
-      return;
+    if (username === "" || password === "" || role === "" || firstname === "" || email === "" || lastname === "") {
+        res.render("auth-views/signup", {
+            message: "All fields are mandatory."
+        });
+        return;
     }
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+    User.findOne({
+        username
+    }, "username", (err, user) => {
+        if (user !== null) {
+            res.render("auth-views/signup", {
+                message: "The username already exists"
+            });
+            return;
+        }
 
-    const newUser = new User({
-      firstname: firstname,
-      lastname: lastname,
-      email: email, 
-      role: role,
-      username: username,
-      password: hashPass,
-    });
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
 
-    newUser.save((err) => {
-      if (err) {
-        res.render("auth-views/signup", {
-          message: "Something went wrong"
+        const newUser = new User({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            role: role,
+            username: username,
+            password: hashPass,
         });
-      } else {
-        res.redirect("/");
-      }
+
+        newUser.save((err) => {
+            if (err) {
+                res.render("auth-views/signup", {
+                    message: "Something went wrong"
+                });
+            } else {
+                res.redirect("/");
+            }
+        });
     });
-  });
 });
 
 authRoutes.get("/dashboard", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("dashboard", {
-    user: req.user
-  });
+    res.render("dashboard", {
+        user: req.user
+    });
 });
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    const role = req.user.role;
-    if (role === 'ADMIN') {
-      res.render('./views/dashboard', {
-        user: req.user.name,
-        role: req.user.role
-      });
-    } else if (role === 'COMPANY') {
-      res.render('./views/dashboard', {
-        user: req.user.name,
-        role: req.user.role
-      });
+    if (req.isAuthenticated()) {
+        const role = req.user.role;
+        if (role === 'ADMIN') {
+            res.render('./views/dashboard', {
+                user: req.user.name,
+                role: req.user.role
+            });
+        } else if (role === 'COMPANY') {
+            res.render('./views/dashboard', {
+                user: req.user.name,
+                role: req.user.role
+            });
+        } else {
+            res.render('./views/dashboard', {
+                user: req.user.name,
+                role: req.user.role
+            })
+        }
     } else {
-      res.render('./views/dashboard', {
-        user: req.user.name,
-        role: req.user.role
-      })
+        res.redirect('/login')
     }
-  } else {
-    res.redirect('/login')
-  }
 }
 //=====================================================================================>
 
@@ -146,55 +146,55 @@ const checkironhacker = checkRoles("IRONHACKER");
 //=====================================================================================>
 // Admin Accesses:
 authRoutes.get('/dashboard', checkAdmin, (req, res, next) => {
-  if (!req.user || req.user.role !== "ADMIN") {
-    next();
-    return;
-  }
-  User.find()
-    .then((usersFromDb) => {
-      res.locals.userList = usersFromDb;
-      res.render('admin-views/user-list', {usersFromDb});
-    })
-    .catch((err) => {
-      next(err);
-    });
+    if (!req.user || req.user.role !== "ADMIN") {
+        next();
+        return;
+    }
+    User.find()
+        .then((usersFromDb) => {
+            res.locals.userList = usersFromDb;
+            res.render('admin-views/user-list', { usersFromDb });
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 authRoutes.post('/create-user', (req, res, next) => {
-  const { firstname, lastname, username, email, password, role } = req.body;
-  const salt = bcrypt.genSaltSync(10);
-  if (password === "") {
-    res.redirect('/dashboard');
-    return;
-  }
-  User.create({ firstname, lastname, username, email, password, role })
-    .then(() => {
-      res.redirect('/dashboard');
-    })
-    .catch((err) => {
-      next(err);
-    });
+    const { firstname, lastname, username, email, password, role } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    if (password === "") {
+        res.redirect('/dashboard');
+        return;
+    }
+    User.create({ firstname, lastname, username, email, password, role })
+        .then(() => {
+            res.redirect('/dashboard');
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 authRoutes.post('/delete-user/:userId', (req, res, next) => {
-  if (req.user.role === "IRONHACKER" && req.user.role === "COMPANY") {
-    console.log("User successfuly deleted");
-    return;
-  } else {
-    if (req.user.role === "ADMIN") {
-      console.log("Cannot delete ADMIN");
-      return;
+    if (req.user.role === "IRONHACKER" && req.user.role === "COMPANY") {
+        console.log("User successfuly deleted");
+        return;
+    } else {
+        if (req.user.role === "ADMIN") {
+            console.log("Cannot delete ADMIN");
+            return;
+        }
     }
-  }
-  
-  let userId = req.params.userId;
-  User.remove({ _id: userId })
-    .then(() => {
-      res.redirect('/admin-views/user-list', userId);
-    })
-    .catch((err) => {
-      next(err);
-    });
+
+    let userId = req.params.userId;
+    User.remove({ _id: userId })
+        .then(() => {
+            res.redirect('/admin-views/user-list', userId);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 //=====================================================================================>
 
@@ -204,89 +204,89 @@ authRoutes.post('/delete-user/:userId', (req, res, next) => {
 //=====================================================================================>
 // Company Accesses:
 authRoutes.get('/dashboard', checkCompany, (req, res) => {
-  User.find({}, (err, users) => {
-    res.render('/employer/profile', {
-      users: users,
-      authUser: req.user
+    User.find({}, (err, users) => {
+        res.render('/employer/profile', {
+            users: users,
+            authUser: req.user
+        });
     });
-  });
 });
 
 authRoutes.get('/dashboard', checkCompany, (req, res) => {
-  console.log(req.user)
-  res.render('employer/users/add', {
-    authUser: req.user
-  });
+    console.log(req.user)
+    res.render('employer/users/add', {
+        authUser: req.user
+    });
 
 });
 
 authRoutes.post('/dashboard', checkCompany, (req, res) => {
-  const { companyName, title, description, duties, objective, qualifications, benefits, email,resume } = req.body;
+    const { companyName, title, description, duties, objective, qualifications, benefits, email, resume } = req.body;
 
-  if (companyName === "" || title === "" || description === "" || duties === "" || objective === "" || qualifications === "" || benefits === "" || email === "" || resume === "") {
-    res.render("../views/dashboard", {
-      message: "Please fill all requested fields to be able to publish your vacancy"
-    });
-    return;
-  }
-
-  User.find({})
-    .then(user => {
-      if (user !== null) {
-        res.render("employer/users/add", {
-          message: "Username is not available"
+    if (companyName === "" || title === "" || description === "" || duties === "" || objective === "" || qualifications === "" || benefits === "" || email === "" || resume === "") {
+        res.render("../views/dashboard", {
+            message: "Please fill all requested fields to be able to publish your vacancy"
         });
         return;
-      }
+    }
 
-      const salt = bcrypt.genSaltSync(10);
-      const hashPass = bcrypt.hashSync(password, salt);
+    User.find({})
+        .then(user => {
+            if (user !== null) {
+                res.render("employer/users/add", {
+                    message: "Username is not available"
+                });
+                return;
+            }
 
-      const newCompany = new User({
-        username,
-        password: hashPass
-      });
+            const salt = bcrypt.genSaltSync(10);
+            const hashPass = bcrypt.hashSync(password, salt);
 
-      newCompany.save((err) => {
-        if (err) {
-          res.render("employer/users/add", {
-            message: "Error, contact site Admin."
-          });
-        } else {
-          res.redirect("/employer/users");
-        }
-      });
-    })
-    .catch(error => {
-      next(error)
-    })
+            const newCompany = new User({
+                username,
+                password: hashPass
+            });
+
+            newCompany.save((err) => {
+                if (err) {
+                    res.render("employer/users/add", {
+                        message: "Error, contact site Admin."
+                    });
+                } else {
+                    res.redirect("/employer/users");
+                }
+            });
+        })
+        .catch(error => {
+            next(error)
+        })
 });
 
 authRoutes.get('/employer/users/:id/edit', checkCompany, (req, res) => {
-  User.findOne({
-    _id: req.params.id
-  }, (err, userItem) => {
-    res.render('employer/users/edit', {
-      userItem: userItem,
-      authUser: req.user
+    User.findOne({
+        _id: req.params.id
+    }, (err, userItem) => {
+        res.render('employer/users/edit', {
+            userItem: userItem,
+            authUser: req.user
+        });
     });
-  });
 });
 
 authRoutes.post('/employer/users/:id/edit', checkCompany, (req, res) => {
-  User.updateOne({
-    _id: req.params.id
-  }, req.body, (err, user) => {
-    res.redirect("/employer/users");
-  });
+    User.updateOne({
+        _id: req.params.id
+    }, req.body, (err, user) => {
+        res.redirect("/employer/users");
+    });
 });
 
 authRoutes.post('/employer/users/:id/delete', checkCompany, (req, res) => {
-  User.deleteOne({
-    _id: req.params.id
-  }, (err, user) => {
-    res.redirect("/employer/users");
-  });
+    User.deleteOne({
+        _id: req.params.id
+    }, (err, user) => {
+        res.redirect("/employer/users");
+    });
 });
 //=====================================================================================>
 
@@ -297,61 +297,61 @@ authRoutes.post('/employer/users/:id/delete', checkCompany, (req, res) => {
 //=====================================================================================>
 // IronHacker Accessess:
 authRoutes.get('/dashboard', (req, res, next) => {
-  User.findById(req.params.userId)
-    .then((userDetails) => {
-      res.locals.userInfo = userDetails;
-      res.render('user-views/user-profile');
-    })
-    .catch((err) => {
-      next(err);
-    });
-  // res.render('user-views/user-profile');
+    User.findById(req.params.userId)
+        .then((userDetails) => {
+            res.locals.userInfo = userDetails;
+            res.render('user-views/user-profile');
+        })
+        .catch((err) => {
+            next(err);
+        });
+    // res.render('user-views/user-profile');
 });
 
 authRoutes.post('/dashboard/:userId', (req, res, next) => {
-  const {
-    firstname,
-    lastname,
-    email,
-    username,
-    password,
-    role
-  } = req.body;
-  User.findByIdAndUpdate(
-      req.params.userId, {
+    const {
         firstname,
         lastname,
         email,
         username,
         password,
         role
-      }, {
-        runValidators: true
-      }
-    )
-    .then(() => {
-      res.redirect(`/user-profile/${req.params.userId}`);
-    })
-    .catch((err) => {
-      next(err);
-    })
+    } = req.body;
+    User.findByIdAndUpdate(
+            req.params.userId, {
+                firstname,
+                lastname,
+                email,
+                username,
+                password,
+                role
+            }, {
+                runValidators: true
+            }
+        )
+        .then(() => {
+            res.redirect(`/user-profile/${req.params.userId}`);
+        })
+        .catch((err) => {
+            next(err);
+        })
 });
 
 authRoutes.get('/view-users', (req, res, next) => {
-  if (!req.user) {
-    next();
-    return;
-  }
-  User.find()
-    .then((usersFromDb) => {
-      res.locals.userList = usersFromDb;
-      res.render('user-views/user-list', { usersFromDb });
+        if (!req.user) {
+            next();
+            return;
+        }
+        User.find()
+            .then((usersFromDb) => {
+                res.locals.userList = usersFromDb;
+                res.render('user-views/user-list', { usersFromDb });
+            })
+            .catch((err) => {
+                next(err);
+            });
     })
-    .catch((err) => {
-      next(err);
-    });
-})
-//=====================================================================================>
+    //=====================================================================================>
 
 
 
@@ -373,10 +373,10 @@ authRoutes.get('/view-users', (req, res, next) => {
 //=====================================================================================>
 // Logout
 authRoutes.get("/logout", (req, res) => {
-  req.logout();
-  req.session.destroy();
-  console.log(req.session);
-  res.redirect("./login");
+    req.logout();
+    req.session.destroy();
+    console.log(req.session);
+    res.redirect("./login");
 });
 //=====================================================================================>
 
